cf0737203b33f12026eac5ac72c232b767251ae1
Andy Low <andyjunchenglow@gmail.com>
2023-03-30 17:28:41 +1100

    feat: setup apollo

--- apps/DDA-server/src/app.module.ts
+++ apps/DDA-server/src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'dndDrive',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      }, 
+      cors: true
+    }),
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
